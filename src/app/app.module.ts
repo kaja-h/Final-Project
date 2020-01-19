45071223db55e0b8429c129a4fc04e123729fb17
kaja-h <karolinaa.haran@gmail.com>
2020-01-19 21:24:38 +0100

    adding header and icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FiltersComponent } from './main/filters/filters.component';
+import { OffersComponent } from './main/offers/offers.component';
+import { MapComponent } from './main/map/map.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        MainComponent,
+        FiltersComponent,
+        OffersComponent,
+        MapComponent
+    ],
   imports: [
     BrowserModule
   ],
