52d679034ffa22adc78951fd9f752d74eadadd84
kaja-h <karolinaa.haran@gmail.com>
2020-02-08 00:04:40 +0100

    offers model

---
+++ src/app/main/body/offers/offers-details/offers-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-offers-details',
+  templateUrl: './offers-details.component.html',
+  styleUrls: ['./offers-details.component.css']
+})
+export class OffersDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
