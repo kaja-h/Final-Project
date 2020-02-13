4260cd588999d5e53d3da29c180a7fb8fe1a6960
kaja-h <karolinaa.haran@gmail.com>
2020-02-13 23:30:08 +0100

    offer-details

--- src/app/main/body/offers/offers.component.ts
+++ src/app/main/body/offers/offers.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {Offers} from './offers.model';
+import {OffersService} from './offers.service';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.component.html',
-  styleUrls: ['./offers.component.css']
+  styleUrls: ['./offers.component.css'],
+  providers: [OffersService]
 })
 export class OffersComponent implements OnInit {
   selectedOffer: Offers;
