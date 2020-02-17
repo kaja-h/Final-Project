f4fc1881b30ac46319fd0c9a845c8879bbcaff30
kaja-h <karolinaa.haran@gmail.com>
2020-02-17 20:00:49 +0100

    leaflet map added

--- src/app/main/body/map/map.component.ts
+++ src/app/main/body/map/map.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+
 export class MapComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  private map;
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.initMap();
+  }
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [51.75, 19.4666],
+      zoom: 6
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+    tiles.addTo(this.map);
+  }
 }
